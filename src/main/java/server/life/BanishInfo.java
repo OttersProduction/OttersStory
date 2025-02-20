326ea27efdf1092e94c132299e21121985d59136
Orhay Benaim <orhaybenaim@gmail.com>
2025-02-20 12:19:23 +0200

    move server to server folder

--- src/main/java/server/life/BanishInfo.java
+++
@@ -1,9 +0,0 @@
-package server.life;
-
-import java.util.Objects;
-
-public record BanishInfo(int map, String portal, String msg) {
-    public BanishInfo {
-        Objects.requireNonNull(portal, "BanishInfo portal");
-    }
-}
